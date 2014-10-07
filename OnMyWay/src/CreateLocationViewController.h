edf47ddce4ecc2a3bc65b2b1bb192b4b7f71c4c0
Godakanda <pgodakanda@MTVL122c120e2.local>
2014-10-06 23:54:04 -0700

    Added CreateLocationViewController class

---
+++ OnMyWay/src/CreateLocationViewController.h
@@ -0,0 +1,13 @@
+//
+//  CreateLocationViewController.h
+//  OnMyWay
+//
+//  Created by Godakanda, Hashan on 10/6/14.
+//  Copyright (c) 2014 Dog Forest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CreateLocationViewController : UITableViewController
+
+@end
